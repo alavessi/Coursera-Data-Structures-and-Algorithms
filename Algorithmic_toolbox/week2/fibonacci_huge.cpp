4493f9e40924e930153e47d04926e8c0b8ad901e
Samir Emirov <84871525+alavessi@users.noreply.github.com>
2021-07-05 01:04:13 +0300

    Create fibonacci_huge.cpp

---
+++ Algorithmic_toolbox/week2/fibonacci_huge.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+long long get_fibonacci_huge(long long n, long long m)
+{
+    std::vector<long long> F(6*m+1);
+    F[0]=0;
+    F[1]=1;
+    for (long long i = 2; i <= 6*m; i++)
+        F[i] = (F[i-1]%m + F[i-2]%m) %m;
+    long long i=1;
+    while ((F[i]!=0 || F[i+1]!=1) && i<6*m)
+        i++;
+    return F[n%i];
+}
+int main()
+{
+    long long n,m;
+    std::cin>>n>>m;
+    std::cout<<get_fibonacci_huge(n, m);
+    return 0;
+}
