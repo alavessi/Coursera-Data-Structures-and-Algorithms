7c2994890bf1045eb4694ea3326bf47fed87504c
Samir Emirov <84871525+alavessi@users.noreply.github.com>
2021-07-08 21:45:59 +0300

    Update fibonacci_sum_squares.cpp

--- Algorithmic_toolbox/week2/fibonacci_sum_squares.cpp
+++ Algorithmic_toolbox/week2/fibonacci_sum_squares.cpp
@@ -2,15 +2,15 @@
 
 int fibonacci_sum_squares_naive(long long n)
 {
-    int F[30],S[30];
+    int F[30], S[30];
     F[0]=0;
     F[1]=1;
     S[0]=0;
     S[1]=1;
     for (int i=2; i<30; i++)
     {
-        F[i]=F[i-1]%10+F[i-2]%10;
-        S[i]=S[i-1]+((F[i]%10)*(F[i]%10))%10;
+        F[i] = F[i-1]%10 + F[i-2]%10;
+        S[i] = S[i-1] + ((F[i]%10) * (F[i]%10)) % 10;
     }
     return S[n%30]%10;
 }
