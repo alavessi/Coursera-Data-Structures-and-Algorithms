fc732e6efc15a172cbfd8498e4ef83d83507a04e
Samir Emirov <84871525+alavessi@users.noreply.github.com>
2021-07-10 18:16:53 +0300

    Create car_fueling.cpp

---
+++ Algorithmic_toolbox/week3/car_fueling.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+int compute_min_refills(int dist, int tank, int n, std::vector<int> &stops)
+{
+    int numRefills=0, currentRefill=0, lastRefill;
+    while(currentRefill <= n)
+    {
+        lastRefill = currentRefill;
+        while(currentRefill <= n && stops[currentRefill+1] - stops[lastRefill] <= tank)
+            currentRefill++;
+        if(currentRefill == lastRefill)
+            return -1;
+        if(currentRefill <= n)
+            numRefills++;
+    }
+    return numRefills;
+}
+main()
+{
+    int d,m,n;
+    std::cin >> d >> m >> n;
+    std::vector<int> stops(n+2);
+    for (int i=1; i<=n; i++)
+        std::cin >> stops[i];
+    stops[0] = 0;
+    stops[n+1] = d;
+    std::cout << compute_min_refills(d, m, n, stops);
+    return 0;
+}
