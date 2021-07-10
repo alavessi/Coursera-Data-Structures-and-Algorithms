542f9f267b104a36d98dd544d6c7d683fb9e4493
Samir Emirov <84871525+alavessi@users.noreply.github.com>
2021-07-10 17:42:08 +0300

    Create fractional_knapsack.cpp

---
+++ Algorithmic_toolbox/week3/fractional_knapsack.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct Item
+{
+    int weight;
+    int value;
+    friend std::istream& operator>> (std::istream& in, Item& item)
+    {
+        in >> item.value >> item.weight;
+        return in;
+    }
+};
+
+double get_optimal_value(int n, int capacity, std::vector<Item> items)
+{
+    double value = 0.0;
+    std::sort(items.begin(), items.end(), [] (Item a, Item b) -> bool {return a.value * b.weight > a.weight * b.value;});
+    int i=0;
+    while (capacity>0 && i<n)
+    {
+        if (items[i].weight < capacity)
+        {
+            value += items[i].value;
+            capacity -= items[i].weight;
+        }
+        else
+        {
+            value += (double)items[i].value * capacity / items[i].weight;
+            capacity = 0;
+        }
+        i++;
+    }
+    return value;
+}
+
+int main()
+{
+    int n, capacity;
+    std::cin >> n >> capacity;
+    std::vector<Item> items(n);
+    for (int i=0; i<n; i++)
+    {
+        std::cin >> items[i];
+    }
+    double optimal_value = get_optimal_value(n, capacity, items);
+    std::cout.precision(10);
+    std::cout << optimal_value;
+}
