c147b450e7d44e96ffcd1dcd0230763adff6e871
alavessi <84871525+alavessi@users.noreply.github.com>
2022-03-11 18:05:04 +0300

    Update lcm.cpp

--- Algorithmic_toolbox/week2/lcm.cpp
+++ Algorithmic_toolbox/week2/lcm.cpp
@@ -2,20 +2,20 @@
 
 int gcd(int a, int b)
 {
-    if(b==0)
-        return a;
-    return gcd(b,a%b);
+    if (!b) return a;
+    return gcd(b, a % b);
 }
 
 long long lcm(long a, long b)
 {
-    long long d=gcd(a,b);
-    return (a/d*b);
+    long long d = gcd(a, b);
+    return (a / d * b);
 }
 
 int main()
 {
-    int a,b;
-    std::cin>>a>>b;
-    std::cout<<lcm(a,b)<<std::endl;
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << lcm(a, b) << std::endl;
+    return 0;
 }
