9f0cb58fcbba146f90970789c578c94a3e9c4f8d
alavessi <84871525+alavessi@users.noreply.github.com>
2022-03-11 18:03:04 +0300

    Update gcd.cpp

--- Algorithmic_toolbox/week2/gcd.cpp
+++ Algorithmic_toolbox/week2/gcd.cpp
@@ -2,14 +2,14 @@
 
 int gcd(int a, int b)
 {
-    if(b==0)
-        return a;
-    return gcd(b,a%b);
+    if(!b) return a;
+    return gcd(b, a % b);
 }
 
 int main()
 {
-    int a,b;
-    std::cin>>a>>b;
-    std::cout<<gcd(a,b)<<std::endl;
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << gcd(a, b) << std::endl;
+    return 0;
 }
