c840954e406323baf3c955d39af90b01a8e3d95f
alavessi <84871525+alavessi@users.noreply.github.com>
2022-03-11 17:52:56 +0300

    Update max_pairwise_product.cpp

--- Algorithmic_toolbox/week1/max_pairwise_product.cpp
+++ Algorithmic_toolbox/week1/max_pairwise_product.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-long long MaxPairwiseProduct(const std::vector <long long>& numbers)
+size_t MaxPairwiseProduct(const std::vector<size_t>& numbers)
 {
-    long long max_product;
-    int i, indmax, indmin, n=numbers.size();
-    long long max1=-10000000000000, max2=-10000000000000, min1=100000000000000, min2=1000000000000000;
-    for (i=0; i<n; i++)
-        if (numbers[i]>max1)
+    size_t max1 = std::max(numbers[0], numbers[1]), max2 = std::min(numbers[0], numbers[1]);
+    for (size_t i = 2; i < numbers.size(); i++)
+    {
+        if (numbers[i] > max1)
         {
-            max1=numbers[i];
-            indmax=i;
+            max2 = max1;
+            max1 = numbers[i];
         }
-    for (i=0; i<n; i++)
-        if (numbers[i]<min1)
+        else if (numbers[i] > max2)
         {
-            min1=numbers[i];
-            indmin=i;
+            max2 = numbers[i];
         }
-    for (i=0; i<n; i++)
-        if (numbers[i]>max2 && i!=indmax)
-            max2=numbers[i];
-    for (i=0; i<n; i++)
-        if (numbers[i]<min2 && i!=indmin)
-            min2=numbers[i];
-    max_product=std::max(max1*max2,min1*min2);
-    return max_product;
+    }
+    return max1 * max2;
 }
 
 int main()
 {
     int n;
-    std::cin>>n;
-    std::vector <long long> numbers(n);
-    for (int i=0; i<n; i++)
-        std::cin>>numbers[i];
-    std::cout<<MaxPairwiseProduct(numbers)<<std::endl;
+    std::cin >> n;
+    std::vector<size_t> numbers(n);
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> numbers[i];
+    }
+    std::cout << MaxPairwiseProduct(numbers) << std::endl;
     return 0;
 }
