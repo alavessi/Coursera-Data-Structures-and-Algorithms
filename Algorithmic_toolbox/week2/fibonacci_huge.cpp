13c9a85ecd40ad24137db56074863036849a4f1e
alavessi <84871525+alavessi@users.noreply.github.com>
2022-03-12 00:09:39 +0300

    Update fibonacci_huge.cpp

--- Algorithmic_toolbox/week2/fibonacci_huge.cpp
+++ Algorithmic_toolbox/week2/fibonacci_huge.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <vector>
 
-long long get_fibonacci_huge(long long n, long long m)
+size_t get_fibonacci_huge(size_t n, size_t m)
 {
-    std::vector<long long> F(6*m+1);
-    F[0]=0;
-    F[1]=1;
-    for (long long i = 2; i <= 6*m; i++)
-        F[i] = (F[i-1]%m + F[i-2]%m) %m;
-    long long i=1;
-    while ((F[i]!=0 || F[i+1]!=1) && i<6*m)
+    std::vector<size_t> F(6 * m + 1);
+    F[0] = 0;
+    F[1] = 1;
+    for (size_t i = 2; i <= 6 * m; i++)
+    {
+        F[i] = (F[i - 1] % m + F[i - 2] % m) % m;
+    }
+    size_t i = 1;
+    while (i < 6 * m && (F[i] || F[i + 1] != 1))
+    {
         i++;
-    return F[n%i];
+    }
+    return F[n % i];
 }
 
 int main()
 {
-    long long n,m;
-    std::cin>>n>>m;
-    std::cout<<get_fibonacci_huge(n, m);
+    size_t n, m;
+    std::cin >> n >> m;
+    std::cout << get_fibonacci_huge(n, m);
     return 0;
 }
