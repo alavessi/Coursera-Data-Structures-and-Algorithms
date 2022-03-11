4a61401bc36d2121b6101e2e0056f7876dcf7f29
alavessi <84871525+alavessi@users.noreply.github.com>
2022-03-12 00:15:51 +0300

    Update fibonacci_sum_last_digit.cpp

--- Algorithmic_toolbox/week2/fibonacci_sum_last_digit.cpp
+++ Algorithmic_toolbox/week2/fibonacci_sum_last_digit.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-int fibonacci_sum_naive(size_t n)
+size_t fibonacci_sum_naive(size_t n)
 {
     if (n <= 1) return n;
-    int F[60] = {0, 1}, S[60] = {0, 1};
-    for (int i = 2; i < 60; i++)
+    size_t F[60] = {0, 1}, S[60] = {0, 1};
+    for (size_t i = 2; i < 60; i++)
     {
         F[i] = F[i - 1] % 10 + F[i - 2] % 10;
         S[i] = S[i - 1] + F[i] % 10;
